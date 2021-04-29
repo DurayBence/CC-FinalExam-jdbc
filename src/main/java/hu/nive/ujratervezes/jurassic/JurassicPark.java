47bfce8ce90b3fdfc60d5c5e65de797c4d69a31b
DurayBence <duray.bence@users.noreply.github.com>
2021-04-29 11:03:22 +0200

    Complete task

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,12 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +17,29 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> overpopulatedSpeciesList = new ArrayList<>();
+
+        try {
+            String SQL = "SELECT breed FROM dinosaur " +
+                    "WHERE actual > expected " +
+                    "ORDER BY breed ASC";
+            PreparedStatement pst = DriverManager.getConnection(dbUrl, dbUser, dbPassword).prepareStatement(SQL);
+
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                overpopulatedSpeciesList.add(rs.getString("breed"));
+            }
+
+            rs.close();
+            pst.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return overpopulatedSpeciesList;
     }
 
 }
